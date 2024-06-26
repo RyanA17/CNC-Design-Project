8c7daa064dcbff2462b0d34e5d53c9792a5c06eb
RyanA17 <ryan.achong@unh.edu>
2024-06-26 16:48:43 -0400

    created C file

---
+++ CNC Drawing.c
@@ -0,0 +1,29 @@
+//
+//  CNC Drawing.c
+//
+//
+//  Created by Ryan A on 6/26/24.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+#define ROW 50 /* max rows */
+#define COL 50 /* max columns */
+
+/*-------------------------
+Functions
+-------------------------*/
+//void circle;
+
+int i;
+int j;
+
+int main(void){
+
+int matrix[i][j];
+printf("Pizza\n");
+printf("University of New Hampshire\n");
+
+return 0;
+}
